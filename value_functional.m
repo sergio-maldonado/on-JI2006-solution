222ae0b3c8abd47877147a5ebb618ebe9ffb44e2
sergio-maldonado <53574986+sergio-maldonado@users.noreply.github.com>
2019-08-07 16:18:07 +0100

    Add files via upload

---
+++ value_functional.m
@@ -0,0 +1,60 @@
+% Computes the value of the functional J[x(h)] (eq. 2)* for a given curve
+%
+% * actually J[h(x)] is computed here instead, with necessary modifications 
+% having been made (i.e. change to an integral in x)
+%
+% Sergio Maldonado 2019
+% University of Southampton
+%
+% supplementary code to:
+% Maldonado, S. and Uchasara, M. (2019) On the thermodynamics-based
+% equilibrium beach profile derived by Jenkins and Inman (2016). arxiv:
+% XXXXXX
+
+clear variables
+clc
+close all
+
+%input data ==============================================
+
+% beach profile:
+dire  = '/Users/sergio/Documents/Southampton/Writings/papers/Beach_profile/data/Jenkins_solutions';
+file = 'cyc_prof_f.csv';
+
+n = 1.479;
+
+%input data ==============================================
+
+cd(dire)
+
+m = -3*(n + 1)/4;
+
+pr = importdata(file);
+
+x = pr(:,1);
+h = pr(:,2);
+
+% central scheme:
+dhdx = (h(3:end) - h(1:end-2))./(x(3:end) - x(1:end-2))./2.0;
+dhdx = [dhdx(1) ; dhdx ; dhdx(end)];
+
+L = (h.^m).*sqrt(1 + dhdx.^2);
+
+% functional is
+F = trapz(x,L);
+
+disp('The value of the functional is:')
+disp(F)
+
+% plots for reference:
+figure
+plot(x,h,'-k')
+title('beach profile')
+xlabel('x (m)')
+ylabel('h (m)')
+
+figure
+plot(x,dhdx)
+title('slope of the profile dh/dx')
+xlabel('x (m)')
+ylabel('dh/dx')
